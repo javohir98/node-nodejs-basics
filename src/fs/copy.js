6bfb04571a9bb300763cc1d0570f9f027c5178de
javohir98 <jerkinov072@gmail.com>
2025-10-27 10:48:41 +0500

    feat: implement file copy functionality with error handling

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,16 @@
+import { existsSync } from "fs";
+import { cp } from "node:fs/promises";
+
 const copy = async () => {
-  // Write your code here
+  if (!existsSync("files_copy")) {
+    try {
+      await cp("files", "files_copy", { recursive: true });
+    } catch (err) {
+      console.error("Error copying folder:", err);
+    }
+  } else {
+    throw Error("FS operation failed");
+  }
 };
 
 await copy();
