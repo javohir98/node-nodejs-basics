58b30f10ffde001f6d72720f8295ff254ff70a49
javohir98 <jerkinov072@gmail.com>
2025-10-27 10:25:28 +0500

    create.js complated

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,13 @@
+import { existsSync, promises as fs } from "fs";
+import { join } from "path";
+
+const filePath = join("files", "fresh.txt");
+
 const create = async () => {
-  // Write your code here
+  if (existsSync(filePath)) {
+    throw Error("FS operation failed");
+  }
+  await fs.writeFile(filePath, "I am fresh and young");
 };
 
 await create();
